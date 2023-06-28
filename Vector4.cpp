a3b14ab217fefdf4b001ab21e817d669ec7e6776
IguchiShunpei <k021g1129@g.neec.ac.jp>
2023-06-28 16:58:29 +0900

    警告をエラー扱いに設定した

--- Vector4.cpp
+++ Vector4.cpp
@@ -1,79 +1,86 @@
 #include "Vector4.h"
 
-#include<cmath>  //sprt
-
-Vector4::Vector4() :x(0), y(0), z(0), w(0)
+Vector4::Vector4()
+	:x(0), y(0), z(0), w(0)
 {
 }
 
-Vector4::Vector4(float x, float y, float z, float w) {
-	this->x = x;
-	this->y = y;
-	this->z = z;
-	this->w = w;
+Vector4::Vector4(float x, float y, float z, float w)
+	: x(x), y(y), z(z), w(w)
+{
 }
 
-float Vector4::length()const {
+float Vector4::Length() const
+{
 	return std::sqrt(x * x + y * y + z * z + w * w);
 }
 
-Vector4& Vector4::normalize() {
-	float len = length();
-	if (len != 0) {
+Vector4& Vector4::Normalize()
+{
+	float len = Length();
+
+	if (len != 0)
+	{
 		return *this /= len;
 	}
+
 	return *this;
 }
 
-float Vector4::dot(const Vector4& v) const
+float Vector4::Dot(const Vector4& v)const
 {
 	return x * v.x + y * v.y + z * v.z + w * v.w;
 }
 
-Vector4 Vector4::cross(const Vector4& v) const
+Vector4 Vector4::Cross(const Vector4& v)const
 {
-	return Vector4(
-		y * v.z - z * v.y,
-		z * v.x - x * v.z,
-		x * v.y - y * v.x,
-		v.w
-	);
+	Vector4 temp;
+
+	temp.x = this->y * v.z - this->z * v.y;
+	temp.y = this->z * v.x - this->x * v.z;
+	temp.z = this->x * v.y - this->y * v.x;
+	temp.w = v.w;
+
+	return temp;
 }
 
-Vector4 Vector4::operator+() const
+Vector4 Vector4::operator+()const
 {
 	return Vector4(x, y, z, w);
 }
 
-Vector4 Vector4::operator-() const
+Vector4 Vector4::operator-()const
 {
-	return Vector4(x, y, z, w);
+	return Vector4(-x, -y, -z, -w);
 }
 
 Vector4& Vector4::operator+=(const Vector4& v)
 {
-	this->x += v.x;
-	this->y += v.y;
-	this->z += v.z;
-	this->w += v.w;
+	x += v.x;
+	y += v.y;
+	z += v.z;
+	w += v.w;
+
 	return *this;
 }
 
 Vector4& Vector4::operator-=(const Vector4& v)
 {
-	this->x -= v.x;
-	this->y -= v.y;
-	this->z -= v.z;
-	this->w -= v.w;
+	x -= v.x;
+	y -= v.y;
+	z -= v.z;
+	w -= v.w;
+
 	return *this;
 }
 
 Vector4& Vector4::operator*=(float s)
 {
-	this->x *= s;
-	this->y *= s;
-	this->z *= s;
-	this->w *= s;
+	x *= s;
+	y *= s;
+	z *= s;
+	w *= s;
+
 	return *this;
 }
 
@@ -83,28 +90,29 @@ Vector4& Vector4::operator/=(float s)
 	y /= s;
 	z /= s;
 	w /= s;
+
 	return *this;
 }
-
-//Vector4 ÉNÉâÉXÇ…ëÆÇ≥Ç»Ç¢ä÷êîåQ
-
 
 const Vector4 operator+(const Vector4& v1, const Vector4& v2)
 {
 	Vector4 temp(v1);
-	return temp + v2;
+
+	return temp += v2;
 }
 
 const Vector4 operator-(const Vector4& v1, const Vector4& v2)
 {
 	Vector4 temp(v1);
-	return temp - v2;
+
+	return temp -= v2;
 }
 
 const Vector4 operator*(const Vector4& v, float s)
 {
 	Vector4 temp(v);
-	return temp * s;
+
+	return temp *= s;
 }
 
 const Vector4 operator*(float s, const Vector4& v)
@@ -115,5 +123,6 @@ const Vector4 operator*(float s, const Vector4& v)
 const Vector4 operator/(const Vector4& v, float s)
 {
 	Vector4 temp(v);
-	return temp / s;
+
+	return temp /= s;
 }
