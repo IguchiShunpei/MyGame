475c59bae46c88aa2b4fec3fdb2212daaa001331
IguchiShunpei <k021g1129@g.neec.ac.jp>
2023-10-10 15:45:46 +0900

    ボス敵を追加

---
+++ DirectXGame/Application/GameObject/BossEnemy.cpp
@@ -0,0 +1,145 @@
+#include "BossEnemy.h"
+#include "SphereCollider.h"
+#include "GamePlayScene.h"
+#include "string.h"
+
+//デストラクタ
+BossEnemy::~BossEnemy()
+{
+	delete enemyModel;
+}
+
+//初期化
+void BossEnemy::BossEnemyInitialize()
+{
+	Initialize();
+	// OBJからモデルデータを読み込む
+	enemyModel = Model::LoadFromOBJ("enemy04");
+	// 3Dオブジェクト生成
+	Create();
+	// オブジェクトにモデルをひも付ける
+	SetModel(enemyModel);
+	isDead_ = false;
+	isDeathTimer_ = false;
+
+	deathTimer_ = 180;
+}
+
+void BossEnemy::Update()
+{
+	isHit_ = false;
+
+	ActiveDeathTimer();
+
+	Move();
+
+	// ワールドトランスフォームの行列更新と転送
+	worldTransform_.UpdateMatrix();
+}
+
+void BossEnemy::ColliderUpdate()
+{
+	//当たり判定更新
+	if (collider)
+	{
+		collider->Update();
+	}
+}
+
+void BossEnemy::OnCollision([[maybe_unused]] const CollisionInfo& info)
+{
+	const char* str1 = "class PlayerBullet";
+
+	//相手がplayerBullet
+	if (strcmp(toCollisionName, str1) == 0)
+	{
+
+		isHit_ = true;
+		hp_--;
+		if (hp_ == 0)
+		{
+			isDeathTimer_ = true;
+		}
+	}
+}
+
+void BossEnemy::Move()
+{
+	if (isDeathTimer_ == false)
+	{
+		switch (phase_)
+		{
+		case Phase::Approach: //接近フェーズ
+		default:
+			//移動(ベクトルを加算)
+			Approach();
+			break;
+		case Phase::Leave:   //離脱フェーズ
+			Leave();
+			break;
+		case Phase::Curve:   //カーブフェーズ
+			Curve();
+			break;
+		case Phase::ReCurve:   //カーブフェーズ
+			ReCurve();
+			break;
+		}
+	}
+}
+
+void BossEnemy::ActiveDeathTimer()
+{
+	isHit_ = false;
+	if (isDeathTimer_ == true)
+	{
+		if (deathTimer_ <= 0)
+		{
+			isDead_ = true;
+		}
+		deathTimer_--;
+	}
+}
+
+void BossEnemy::Approach()
+{
+	worldTransform_.position_ += approach_;
+
+	//既定の位置に着いたら離脱へ
+	if (worldTransform_.position_.z <= 30.0f)
+	{
+		phase_ = Phase::Leave;
+	}
+}
+
+void BossEnemy::Leave()
+{
+	worldTransform_.position_ += leave_;
+
+	//既定の位置に着いたらカーブへ
+	if (worldTransform_.position_.z >= 100.0f)
+	{
+		phase_ = Phase::Approach;
+	}
+}
+
+void BossEnemy::Curve()
+{
+	MathFunc::CurveProjection(worldTransform_, startSpeed, C, flame);
+}
+
+void BossEnemy::ReCurve()
+{
+	MathFunc::CurveProjection(worldTransform_, { -startSpeed.x,startSpeed.y,startSpeed.z }, -C, flame);
+}
+
+
+Vector3 BossEnemy::GetPosition()
+{
+	Vector3 worldPos;
+
+	worldPos.x = worldTransform_.position_.x;
+	worldPos.y = worldTransform_.position_.y;
+	worldPos.z = worldTransform_.position_.z;
+
+	return worldPos;
+}
---
+++ DirectXGame/Application/GameObject/BossEnemy.h
@@ -0,0 +1,117 @@
+#pragma once
+#include "Object3d.h"
+#include "WinApp.h"
+#include "Vector3.h"
+#include "Model.h"
+#include "EnemyBullet.h"
+#include <memory>
+#include <list>
+
+//GameSceneの前方宣言
+class GameScene;
+
+class BossEnemy : public Object3d
+{
+	//移動パターン
+	enum class Phase
+	{
+		None,
+		Approach,//接近
+		Leave,   //離脱
+		Curve,   //カーブ
+		ReCurve, //逆方向カーブ
+	};
+
+public:
+	//デストラクタ
+	~BossEnemy();
+	//初期化
+	void BossEnemyInitialize();
+
+	//更新
+	void Update();
+
+	//当たり判定更新
+	void ColliderUpdate();
+
+	//getter
+	//IsDeathTimer
+	bool GetIsDeathTimer() const { return isDeathTimer_; }
+	//IsDead
+	bool GetIsDead() const { return isDead_; }
+	//IsHit
+	bool GetIsHit() const { return isHit_; }
+	//HP
+	int GetHP() const { return hp_; }
+	//DeathTimer
+	int GetDeathTimer() const { return deathTimer_; }
+	
+	//当たり判定コールバック
+	void OnCollision(const CollisionInfo& info) override;
+	
+	//移動
+	void Move();
+
+	//死亡するまでのタイマー処理
+	void ActiveDeathTimer();
+
+	//接近
+	void Approach();
+	//離脱
+	void Leave();
+	//カーブ
+	void Curve();
+	void ReCurve();
+
+	//Setter
+	//HP
+	void SetHp(int hp) { this->hp_ = hp; }
+	//gameScene
+	void SetGameScene(GameScene* gameScene) { gameScene_ = gameScene; }
+	//phase
+	void SetPhase(Phase phase) { phase_ = phase; }
+	//deathTimer
+	void SetDeathTimer(int deathTimer) { this->deathTimer_ = deathTimer; }
+
+	//ワールド座標を取得
+	Vector3 GetPosition();
+
+private:
+	//ゲームシーン
+	GameScene* gameScene_ = nullptr;
+
+	//デスフラグ
+	bool isDeathTimer_ = false;
+	//倒されたか
+	bool isDead_ = false;
+	//弾が当たったか
+	bool isHit_ = false;
+
+	// モデル
+	Model* enemyModel = nullptr;
+
+	//弾を打ち出すまでの時間
+	float dalayTimer_ = 15.0f;
+
+	//死亡するまでの時間
+	int deathTimer_ = 180;
+
+	//接近速度
+	Vector3 approach_ = { 0.0f,0.0f,-0.3f };
+
+	//離脱速度
+	Vector3 leave_ = { 0.0f,0.0f,1.0f };
+	
+	//敵の移動パターン
+	Phase phase_ = Phase::None;
+
+	//体力
+	int hp_;
+
+	//曲がる大きさ
+	const float C = 0.5f;
+	//初速
+	Vector3 startSpeed = { -0.5f,0.0f,-0.5f };
+	//落下時間
+	float flame = 0.0f;
+};
