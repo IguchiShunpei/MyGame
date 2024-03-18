412c5c693340e86233fff16018a6c78d724eac2f
IguchiShunpei <k021g1129@g.neec.ac.jp>
2024-03-18 19:23:46 +0900

    敵の汎用的な処理をまとめたクラスの作成

---
+++ DirectXGame/Application/GameObject/EnemyObject/ShotEnemy.cpp
@@ -0,0 +1,249 @@
+/**
+* @file ShotEnemy.cpp
+* @brief 弾を発射する敵の処理クラス
+* @author イグチ_シュンペイ
+*/
+
+#include "ShotEnemy.h"
+#include "SphereCollider.h"
+#include "GamePlayScene.h"
+#include "string.h"
+#include "Vector3.h"
+
+//デストラクタ
+ShotEnemy::~ShotEnemy()
+{
+
+}
+
+//初期化
+void ShotEnemy::ShotEnemyInitialize()
+{
+	Initialize();
+	// OBJからモデルデータを読み込む
+	enemyModel_01 = Model::LoadFromOBJ("enemy");
+	// 3Dオブジェクト生成
+	Create();
+	// オブジェクトにモデルをひも付ける
+	SetModel(enemyModel_01.get());
+
+	bulletColliderPos_ = { 0.0f,0.0f,0.0f };
+	bulletColliderRadius_ = 0.5f;
+
+	kBulletSpeed_ = 1.0f;
+
+	dalayTimer_ = 100.0f;
+	delayTImerMax_ = 15.0f;
+	isDead_ = false;
+	isDelete_ = false;
+	isHit_ = false;
+	isInit_ = false;
+	initTime_ = 0.0f;
+	initTimeMax_ = 180.0f;
+	initY_ = 60.0f;
+	afterInitY_ = 60.0f;
+	bulletNum_ = 0;
+	bulletMax_ = 3;
+	enemyColor_ = { 0.0f,0.0f,0.0f };
+	originalColor_ = { 1.0f,1.0f,1.0f };
+	changeColor_ = { 3.0f,3.0f,3.0f };
+	moveLen_ = 30.0f;
+	moveTime_ = 0.0f;
+	moveTimeMax_ = 345.0f;
+}
+
+void ShotEnemy::ShotEnemyUpdate(Vector3 playerPos)
+{
+	isHit_ = false;
+	enemyColor_ = originalColor_;
+	changeColor_ = { 3.0f,3.0f,3.0f };
+
+	//デスフラグの立った弾を削除
+	bullets_.remove_if([](std::unique_ptr < EnemyBullet>& bullet)
+		{
+			return bullet->GetIsDelete();
+		});
+
+	//登場モーション
+	InitMotion();
+	if (isInit_ == true)
+	{
+		Move();
+		//攻撃したら退場
+		if (isAttack_ == false)
+		{
+			Attack(playerPos);
+		}
+	}
+
+	//退場モーション
+	BackMotion();
+
+	//弾更新
+	for (std::unique_ptr<EnemyBullet>& bullet : bullets_)
+	{
+		bullet->Update();
+		bullet->ColliderUpdate();
+	}
+
+	// ワールドトランスフォームの行列更新と転送
+	worldTransform_.UpdateMatrix();
+}
+
+void ShotEnemy::ColliderUpdate()
+{
+	//当たり判定更新
+	if (collider)
+	{
+		collider->Update();
+	}
+}
+
+void ShotEnemy::Damage(int damage)
+{
+	//攻撃を受けたときに変色
+	enemyColor_ = changeColor_;
+	//ダメージ処理
+	hp_ -= damage;
+	if (hp_ <= 0)
+	{
+		isDead_ = true;
+	}
+}
+
+void ShotEnemy::OnCollision([[maybe_unused]] const CollisionInfo& info)
+{
+	const char* str1 = "class PlayerBullet";
+
+	//相手がplayerBullet
+	if (strcmp(toCollisionName, str1) == 0)
+	{
+		isHit_ = true;
+		Damage(damage_);
+	}
+}
+
+void ShotEnemy::Move()
+{
+	if (isBack_ == false)
+	{
+		if (moveTime_ <= moveTimeMax_)
+		{
+			//csvファイルから読み取った方向に移動
+			switch (moveNum_)
+			{
+			case MoveNum::Up:   //カーブフェーズ
+				worldTransform_.position_.y = beforeMovePos_.y + (moveLen_ * MathFunc::easeOutSine(moveTime_ / moveTimeMax_));
+				break;
+			case MoveNum::Down:   //カーブフェーズ
+				worldTransform_.position_.y = beforeMovePos_.y - (moveLen_ * MathFunc::easeOutSine(moveTime_ / moveTimeMax_));
+				break;
+			case MoveNum::Right:   //カーブフェーズ
+				worldTransform_.position_.x = beforeMovePos_.x + (moveLen_ * MathFunc::easeOutSine(moveTime_ / moveTimeMax_));
+				break;
+			case MoveNum::Left:   //カーブフェーズ
+				worldTransform_.position_.x = beforeMovePos_.x - (moveLen_ * MathFunc::easeOutSine(moveTime_ / moveTimeMax_));
+				break;
+			}
+			moveTime_++;
+		}
+		else
+		{
+			//移動しきったら退場モーションに使うy座標を更新
+			beforeY_ = worldTransform_.position_.y;
+			isBack_ = true;
+		}
+	}
+}
+
+void ShotEnemy::Attack(Vector3 playerPos_)
+{
+	dalayTimer_--;
+
+	//プレイヤーのワールド座標の取得
+	Vector3 playerPosition;
+	playerPosition = playerPos_;
+	//自キャラの座標をコピー
+	Vector3 enemyPosition = GetPosition();
+
+	//自機の座標との差分ベクトルを求める
+	velocity_ = enemyPosition - playerPosition;
+
+	//長さを求める
+	velocity_.length();
+	//正規化
+	velocity_.normalize();
+	//ベクトルの長さを,速さに合わせる
+	velocity_ *= kBulletSpeed_;//これが速度になる
+
+	//クールタイムが０になったとき
+	if (dalayTimer_ <= 0)
+	{
+		//球の生成
+		std::unique_ptr<EnemyBullet> newBullet = std::make_unique<EnemyBullet>();
+		//球の初期化
+		newBullet->EnemyBulletInitialize(enemyPosition, velocity_);
+
+		//コライダーの追加
+		newBullet->SetCollider(new SphereCollider(bulletColliderPos_, bulletColliderRadius_));
+
+		//球の登録
+		bullets_.push_back(std::move(newBullet));
+
+		dalayTimer_ = delayTImerMax_;
+		bulletNum_++;
+		//最大数発射するまで繰り返す
+		if (bulletNum_ > bulletMax_)
+		{
+			isAttack_ = true;
+		}
+	}
+}
+
+void ShotEnemy::InitMotion()
+{
+	if (isInit_ == false)
+	{
+		worldTransform_.position_.y = beforeY_ + (afterInitY_ - (initY_ * MathFunc::easeOutBack(initTime_ / initTimeMax_)));
+		initTime_++;
+		if (initTime_ > initTimeMax_)
+		{
+			isInit_ = true;
+			initTime_ = 0.0f;
+			beforeMovePos_ = worldTransform_.position_;
+		}
+	}
+}
+
+void ShotEnemy::BackMotion()
+{
+	if (isBack_ == true)
+	{
+		worldTransform_.position_.y = beforeY_ + initY_ * MathFunc::easeInBack(initTime_ / initTimeMax_);
+		initTime_++;
+		if (initTime_ > initTimeMax_)
+		{
+			isBack_ = true;
+			isDelete_ = true;
+		}
+	}
+}
+
+void ShotEnemy::BulletDraw(ViewProjection* viewProjection_)
+{
+	for (std::unique_ptr<EnemyBullet>& bullet : bullets_)
+	{
+		bullet->Draw(viewProjection_, 1.0f, bullet->GetColor());
+	}
+}
+
+Vector3 ShotEnemy::GetPosition()
+{
+	Vector3 worldPos;
+
+	worldPos.x = worldTransform_.position_.x;
+	worldPos.y = worldTransform_.position_.y;
+	worldPos.z = worldTransform_.position_.z;
+
+	return worldPos;
+}
--- DirectXGame/Application/GameObject/EnemyObject/ShotEnemy.h
+++ DirectXGame/Application/GameObject/EnemyObject/ShotEnemy.h
@@ -1,6 +1,6 @@
 /**
-* @file Enemy.cpp
-* @brief 汎用的な敵の処理クラス
+* @file ShotEnemy.cpp
+* @brief 弾を発射する敵の処理クラス
 * @author イグチ_シュンペイ
 */
 
@@ -10,19 +10,33 @@
 #include "WinApp.h"
 #include "Vector3.h"
 #include "Model.h"
+#include "EnemyBullet.h"
 #include <memory>
+#include <list>
 
-
-class Enemy : public Object3d
+class ShotEnemy : public Object3d
 {
 public:
+	//移動パターン
+	enum MoveNum
+	{
+		Up,
+		Down,
+		Right,
+		Left,
+	};
+
+public:
 	//デストラクタ
-	~Enemy();
+	~ShotEnemy();
 	//初期化
-	void EnemyInitialize();
+	void ShotEnemyInitialize();
 
 	//更新
-	void EnemyUpdate();
+	void ShotEnemyUpdate(Vector3 playerPos);
+
+	//当たり判定更新
+	void ColliderUpdate();
 
 	//ダメージ
 	void Damage(int damage);
@@ -38,9 +52,14 @@ public:
 	int GetHP() const { return hp_; };
 	//color
 	Vector3 GetColor() const { return enemyColor_; }
-
+	
 	//当たり判定コールバック
 	void OnCollision(const CollisionInfo& info) override;
+
+	void Move();
+
+	//攻撃
+	void Attack(Vector3 playerPos_);
 
 	//登場モーション
 	void InitMotion();
@@ -48,9 +67,17 @@ public:
 	//退場モーション
 	void BackMotion();
 
+	//弾を描画
+	void BulletDraw(ViewProjection* viewProjection_);
+
+	//弾リスト
+	const std::list<std::unique_ptr<EnemyBullet>>& GetBullets() { return bullets_; }
+
 	//Setter
 	//HP
 	void SetHp(int hp) { this->hp_ = hp; }
+	//phase
+	void SetMove(MoveNum moveNum) { moveNum_ = moveNum; }
 	//isDead
 	void SetIsDead(bool isDead) { this->isDead_ = isDead; }
 	//beforeY
@@ -87,6 +114,34 @@ private:
 	//攻撃したか
 	bool isAttack_ = false;
 
+	// モデル
+	std::unique_ptr <Model> enemyModel_01;
+	//弾
+	std::unique_ptr <EnemyBullet> enemyBullet_;
+
+	//弾リスト
+	std::list<std::unique_ptr<EnemyBullet>> bullets_;
+
+	//弾の速度
+	float kBulletSpeed_;
+	//移動量
+	Vector3 velocity_;
+
+	//弾を打ち出すまでの時間
+	float dalayTimer_;
+	//最大
+	float delayTImerMax_;
+
+	//弾のcollider
+	Vector3 bulletColliderPos_;
+	float bulletColliderRadius_;
+	
+	//敵の移動パターン
+	MoveNum moveNum_;
+
+	//移動量
+	float moveLen_;
+
 	//体力
 	int hp_;
 
@@ -97,9 +152,19 @@ private:
 	float initY_;
 	float afterInitY_;
 
+	//弾の数
+	int bulletNum_;
+
+	//弾の最大数
+	int bulletMax_;
+
 	//ダメージ
 	int damage_;
 
 	//登場後の座標
 	Vector3 beforeMovePos_;
+
+	//移動時間
+	float moveTime_;
+	float moveTimeMax_;
 };
